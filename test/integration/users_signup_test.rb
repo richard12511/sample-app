cc88624be5214e9303bf5051fa280d81d7463e84
richard12511 <richardnew12511@gmail.com>
2017-03-24 10:29:29 -0500

    add integration test for successful signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,4 +13,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
   end
 
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Test Man",
+                                         email: "testman@gmail.com",
+                                         password: "password123",
+                                         password_confirmation: "password123" }}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+
 end
