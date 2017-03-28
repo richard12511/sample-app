bcbe421e0e32d226bbe75c34faf07abab5f50527
richard12511 <richardnew12511@gmail.com>
2017-03-28 14:18:04 -0500

    add conditional links based on logged_in status

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,8 +2,13 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get new" do
+  test 'should get new' do
     get signup_path
+    assert_response :success
+  end
+
+  test 'should get index' do
+    get users_path
     assert_response :success
   end
 
