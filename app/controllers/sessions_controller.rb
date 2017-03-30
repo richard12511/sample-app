4146dd1b443ebb2b51889d9204c514803ce5728d
richard12511 <richardnew12511@gmail.com>
2017-03-30 11:18:35 -0500

    add persistent login and fix bug where login link points to signup path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -16,6 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out
-    render 'new'
+    redirect_to root_path
   end
 end
