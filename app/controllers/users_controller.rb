89959d9307bfff2a477d1a1e81993051756fec1e
richard12511 <richardnew12511@gmail.com>
2017-04-05 10:29:50 -0500

    add auth to user edit and update routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def index
     @users = User.all
   end
@@ -40,5 +42,12 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = 'Please log in'
+        redirect_to login_url
+      end
     end
 end
